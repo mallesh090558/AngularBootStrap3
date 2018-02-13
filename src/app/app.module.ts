c7d6eb23c3b6c0686376ea744551e598bb6b4dff
Mallesh <mallesh090558@gmail.com>
2018-02-13 15:10:35 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
+  { path: '**', component: ErrorPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent,
+    MainComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
